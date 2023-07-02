2ba641a689308151fc290bddf73c4a7970b28b03
AddisBelayhu <addisgithub@gmail.com>
2023-07-02 15:52:43 +0300

    create fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,13 +4,16 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+//Import DB and Faker services\
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
         // \App\Models\User::factory(10)->create();
 
@@ -18,5 +21,16 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $faker = Faker::create();
+        foreach (range(1,500) as $index) {
+            DB::table('employees')->insert(
+                [
+                    'firstname' => $faker->firstname,
+                    'lastname' => $faker -> lastname,
+                    'email' => $faker -> email,
+                    'dob' => $faker -> date($format = 'D-m-y', $max = '2010', $min = '1980')
+                ]
+            );
+        }
     }
 }
